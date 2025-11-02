cc7af0f7590e85a8d91755c5fbc786f97e8c838c
Sri-Hasnika <srihasnika@gmail.com>
2025-11-02 07:24:09 +0530

    Added tests

---
+++ app/webrtc/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from "react"
+import CallWidget from "@/components/webrtc/CallWidget"
+
+export default function Page() {
+  return (
+    <div>
+      <CallWidget />
+    </div>
+  )
+}
